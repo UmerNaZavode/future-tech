17795a9ff9256267203a213e1e67c967e6dd5219
Gaba <malkocmax@gmail.com>
2025-06-13 15:40:30 +0300

    blog page and blog-details section finished

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ import './../scss/main.scss'
 import Header from './Header.js'
 import TabsCollection from "./Tabs.js";
 import VideoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
 
 new Header()
 new TabsCollection()
 new VideoPlayerCollection ()
+new ExpandableContentCollection()
